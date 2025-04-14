ce0c1dd508435df34edcdf47dad4f8aafc86800d
Sathiya priyan <ssathiya.cs@gmail.com>
2025-04-14 12:33:10 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text } from 'react-native';
+import './global.css';
+
+export default function App() {
+  return (
+    <>
+      <Text className="text-red-500">hisssdfasdfsadfsadfsdf</Text>
+      <StatusBar style="auto" />
+    </>
+  );
+}
